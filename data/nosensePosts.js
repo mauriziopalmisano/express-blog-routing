ea9568f821ef89b352e5cc0fa4a6a7dae71015b1
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-05-22 15:10:36 +0200

    file iniziali

---
+++ data/nosensePosts.js
@@ -0,0 +1,37 @@
+export const SERVER_PORT = process.env.SERVER_PORT;
+export const SERVER_URL = process.env.SERVER_URL;
+
+
+
+export const postList = [
+    {
+        titolo: "Alla scoperta del trekking urbano",
+        contenuto: "Unire il piacere della camminata all'esplorazione delle meraviglie storiche nascoste nelle nostre città. Ecco i 5 percorsi migliori da fare a piedi questo weekend.",
+        immagine: `urban_tracking.png`,
+        tags: ["viaggi", "outdoor", "benessere", "città"]
+    },
+    {
+        titolo: "Introduzione a Vue.js 3",
+        contenuto: "Vue.js continua a essere uno dei framework JavaScript più amati dagli sviluppatori. In questo articolo analizziamo la Composition API e i vantaggi in termini di performance.",
+        immagine: `vue.js_3.png`,
+        tags: ["programmazione", "javascript", "frontend", "webdev"]
+    },
+    {
+        titolo: "I segreti per un caffè perfetto a casa",
+        contenuto: "Dalla scelta della macinatura alla temperatura dell'acqua: tutti i trucchi dei baristi professionisti per estrarre il massimo del gusto anche con la classica moka.",
+        immagine: `guida_caffé.png`,
+        tags: ["cucina", "caffè", "consigli", "casa"]
+    },
+    {
+        titolo: "Guida minimalista alla produttività",
+        contenuto: "Fare di meno per fare meglio. Spesso riempiamo le nostre giornate di task inutili; scopriamo come il metodo 'Essentialism' può rivoluzionare il tuo flusso di lavoro.",
+        immagine: `guida-minimalista.png`,
+        tags: ["crescita personale", "produttività", "lavoro"]
+    },
+    {
+        titolo: "Lancio del nuovo telescopio spaziale",
+        contenuto: "Gli scienziati celebrano il successo del lancio in orbita del telescopio di nuova generazione. Le prime immagini catturate promettono di svelare i segreti della materia oscura.",
+        immagine: `telescopio_spaziale.png`,
+        tags: ["scienza", "spazio", "tecnologia", "astronomia"]
+    }
+];
